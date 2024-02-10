a9f2dc35594aeff3e2c4614e0c63f3c5bff9e006
Dinto2 <reimondid@protonmail.com>
2024-02-10 08:28:19 -0300

    Final1

--- myscript.js
+++ myscript.js
@@ -92,4 +92,29 @@ function addNewClass(elem){
 
 }
 
+let slideIndex4 = 1;
+showSlides4(slideIndex);
 
+function plusSlides4(n) {
+  showSlides4(slideIndex += n);
+}
+
+function currentSlide4(n) {
+  showSlides4(slideIndex = n);
+}
+
+function showSlides4(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides4");
+  let dots = document.getElementsByClassName("dot4");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
